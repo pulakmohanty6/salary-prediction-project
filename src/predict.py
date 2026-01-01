2c4c6e0483f0642cf6dee78ff010b7feb1b8a43e
Pulak Mohanty <mohanty.pulak92@gmail.com>
2026-01-02 00:22:58 +0530

    add/remove print

--- src/predict.py
+++ src/predict.py
@@ -3,7 +3,7 @@ import pandas as pd
 import os
 
 def predict_salary():
-    # 1. Load the saved model
+    # 1. Loading the saved model
 
     if not os.path.exists('models/salary_model.pkl'):
         print("❌ Error: Model not found. Please run main.py first to train it!")
@@ -11,9 +11,9 @@ def predict_salary():
 
     print("Loading saved model...")
     model = joblib.load('models/salary_model.pkl')
-    print("✅ Model loaded!")
+    print("Model loaded!")
 
-    # 2. Ask User for Input
+    # 2. Asking User for Input
 
     user_input = input("\nEnter Years of Experience: ")
     
@@ -23,17 +23,17 @@ def predict_salary():
         print("❌ That doesn't look like a number. Please try again.")
         return
 
-    # 3. Prepare Data (Wrap it in a DataFrame)
+    # 3. Preparing Data
  
     new_data = pd.DataFrame({'YearsExperience': [years]})
 
-    # 4. Predict
+    # 4. Predicting
 
     prediction = model.predict(new_data)
     
-    # 5. Show Result
+    # 5. Showing Result
 
-    print(f"💰 Predicted Salary: ${prediction[0]:,.0f}")
+    print(f"Predicted Salary: ${prediction[0]:,.0f}")
 
 if __name__ == "__main__":
     predict_salary()
